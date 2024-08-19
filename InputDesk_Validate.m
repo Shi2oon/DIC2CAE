2462139775fe3609ea48d48ad615b410cfe31422
Abdo Koko <45297426+Shi2oon@users.noreply.github.com>
2024-08-19 09:22:27 +0100

    Update InputDesk_Validate.m

--- InputDesk_Validate.m
+++ InputDesk_Validate.m
@@ -3,5 +3,5 @@ clc;clear;close all
 addpath(genpath([pwd '\functions']));
 % put the KI, KII, KII values for stereo-DIC data as in
 % Calibration_2DKIII(KI,KII,KIII);for 2D, put KIII as zerp
-Maps = Calibration_2DKIII(5,1,0);
+Maps = Calibration_2DKIII(5,1,3);
 [J,KI,KII,KIII] = DIC2CAE(Maps);
